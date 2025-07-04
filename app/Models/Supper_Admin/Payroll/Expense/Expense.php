ce0ad41cd34ce9d852dd78455ca8e182acfea12c
Sohana Kabir Barna <borna350@gmail.com>
2025-07-04 14:16:08 +0600

    expense category and item model, migration

---
+++ app/Models/Supper_Admin/Payroll/Expense/Expense.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Supper_Admin\Payroll\Expense;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    //
+}
---
+++ app/Models/Supper_Admin/Payroll/Expense/ExpenseItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Supper_Admin\Payroll\Expense;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseItem extends Model
+{
+    protected $fillable =
+        [
+            'expense_category_id',
+            'expense_item_name',
+            'note',
+            'status'
+        ];
+}
