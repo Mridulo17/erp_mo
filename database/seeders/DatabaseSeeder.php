c24e55bc9e67c8bbcad310ffa8f5335ca3f27574
shohel <gsshohel1314@gmail.com>
2025-07-06 17:06:52 +0600

    DatabaseSeeder modify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Profession;
+use App\Models\Admin\Relation;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            BloodGroupTableSeeder::class,
+            GenderTableSeeder::class,
+            ProfessionTableSeeder::class,
+            RelationTableSeeder::class,
+            ReligionTableSeeder::class,
+            // Add other seeders here
         ]);
     }
 }
