2bd10eb728c68814684463511c06dfc4e70d64eb
Sohana Kabir Barna <borna350@gmail.com>
2025-07-10 01:52:47 +0600

    mobile allowance done

--- app/Http/Controllers/Supper_Admin/Payroll/MobileAllowanceController.php
+++ app/Http/Controllers/Supper_Admin/Payroll/MobileAllowanceController.php
@@ -3,64 +3,65 @@
 namespace App\Http\Controllers\Supper_Admin\Payroll;
 
 use App\Http\Controllers\Controller;
+use App\Models\Supper_Admin\Payroll\IncAndDec;
 use App\Models\Supper_Admin\Payroll\MobileAllowance;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class MobileAllowanceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $mobileAllowances = MobileAllowance::get();
+        return view('supper_admin.pages.payroll.mobile-allowance', compact('mobileAllowances'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'department_id'      => 'required|integer',
+                'employee_id'      => 'required|integer',
+                'impression_type'    => 'required|in:Increment,Decrement',
+                'start_month'      => 'required|string',
+                'amount_type'    => 'required|in:Percentage,Fixed',
+                'amount'      => 'required'
+            ]);
+            MobileAllowance::create([
+                'department_id'      => $request->input('department_id'),
+                'employee_id'      => $request->input('employee_id'),
+                'impression_type'      => $request->input('impression_type'),
+                'start_month'      => $request->input('start_month'),
+                'amount_type'      => $request->input('amount_type'),
+                'amount'      => $request->input('amount'),
+                'note'  => $request->input('note')
+            ]);
+            return response()->json(['status' => 'success', 'message' => 'Mobile Allowance added Successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['status' => 'fail', 'message' => $e->validator->errors()]);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'fail', 'message' => $e->getMessage()]);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(MobileAllowance $mobileAllowance)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(MobileAllowance $mobileAllowance)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, MobileAllowance $mobileAllowance)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(MobileAllowance $mobileAllowance)
+    public function destroy(string $id)
     {
-        //
+        try {
+            $mobileAllowance = MobileAllowance::findOrFail($id);
+            $mobileAllowance->delete();
+            return response()->json(['status' => 'success', 'message' => 'Mobile allowance deleted successfully']);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'fail', 'message' => $e->getMessage()]);
+        }
     }
 }
