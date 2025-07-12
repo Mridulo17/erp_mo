229acc5ff19f3f5e87dd085fdf0ac32bc8a9c551
Sohana Kabir Barna <borna350@gmail.com>
2025-07-13 02:59:48 +0600

    sponsor all model, migrtion

--- database/migrations/2025_07_12_202829_create_marketing_visas_table.php
+++ database/migrations/2025_07_12_202829_create_marketing_visas_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('marketing_visas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('id')->on('countries')->cascadeOnUpdate()->restrictOnDelete();
+            $table->unsignedBigInteger('job_list_id');
+            $table->foreign('job_list_id')->references('id')->on('job_lists')->cascadeOnUpdate()->restrictOnDelete();
+            $table->enum('type', ['Air Ticket', 'Business Visa', 'Hazz & Umrah', 'Manpower', 'Patient', 'Tourist', 'Visa processing', 'Worker']);
+            $table->enum('gender', ['Male', 'Female', 'Haji']);
+            $table->integer('salary_currency_id')->unsigned();
+            $table->foreign('salary_currency_id')->references('id')->on('currencies')->onDelete('cascade');
+            $table->decimal('monthly_salary', 25, 2);
+            $table->decimal('salary_bdt_amount', 25, 2);
+            $table->integer('cost_currency_id')->unsigned()->nullable();
+            $table->foreign('cost_currency_id')->references('id')->on('currencies')->onDelete('cascade');
+            $table->decimal('cost', 25, 2)->nullable();
+            $table->decimal('cost_bdt_amount', 25, 2)->nullable();
+            $table->integer('available_qty')->nullable();
+            $table->decimal('registration_fee', 25, 2)->nullable();
+            $table->integer('send_sms_to_agent')->default(0);
+            $table->string('attachment')->nullable();
+            $table->string('note', 2000)->nullable();
+            $table->enum('status', ['Enabled', 'Disabled'])->default('Enabled');
             $table->timestamps();
         });
     }
